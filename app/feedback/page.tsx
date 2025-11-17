ecc5b3708b2e24dca26dfbfe72f064290ca3ddbc
kzoeps <kzoepa@gmail.com>
2025-11-17 19:44:11 +0600

    feat: testinga nd docs

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -11,15 +11,15 @@ export const metadata: Metadata = {
 };
 
 export default async function FeedbackView() {
+	// currently kept the limit as 100 without any pagination. once we cross 15-20 entries we can create an infinite pagination/client pagination
 	const feedbackList = (await getFeedback({
 		limit: 100,
 		offset: 0,
 	})) as Feedback[];
 
 	return (
-		<main className="mx-auto flex max-w-7xl flex-col gap-4 pb-[64px] md:pb-0">
-			<h1 className="mt-6 font-extrabold text-lg">Feedback</h1>
-
+		<main className="mx-auto my-6 flex max-w-7xl flex-col gap-4 px-5 pb-[64px] md:pb-0">
+			<h1 className="font-extrabold text-lg">Feedback</h1>
 			{feedbackList.length === 0 ? (
 				<p className="text-slate-500 text-sm">No feedback yet.</p>
 			) : (
