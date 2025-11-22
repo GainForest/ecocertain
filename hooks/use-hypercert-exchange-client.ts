07ca86799f3320edc1d1e13660909ba69b8928bd
kzoeps <kzoepa@gmail.com>
2025-11-22 15:51:39 +0600

    fix: add a try catch block

--- hooks/use-hypercert-exchange-client.ts
+++ hooks/use-hypercert-exchange-client.ts
@@ -8,32 +8,38 @@ import { celo } from "viem/chains";
 import { useChainId, useWalletClient } from "wagmi";
 
 // different from wagmi config since wagmi config needs to consider for swaps
-const EXCHANGE_SUPPORTED_CHAINS = [celo]
+const EXCHANGE_SUPPORTED_CHAINS = [celo];
 
 export const useHypercertExchangeClient = () => {
-  const { data: walletClient } = useWalletClient();
-  const chainId = useChainId();
-  const provider = useEthersProvider();
-  const signer = useEthersSigner();
+	const { data: walletClient } = useWalletClient();
+	const chainId = useChainId();
+	const provider = useEthersProvider();
+	const signer = useEthersSigner();
 
-  const client = useMemo(() => {
-    if (
-      !EXCHANGE_SUPPORTED_CHAINS.find((chain) => chain.id === walletClient?.chain.id)
-    ) {
-      return null;
-    }
+	const client = useMemo(() => {
+		if (
+			!EXCHANGE_SUPPORTED_CHAINS.find(
+				(chain) => chain.id === walletClient?.chain.id,
+			)
+		) {
+			return null;
+		}
 
-    return new HypercertExchangeClient(
-      chainId,
-      // @ts-expect-error - wagmi and viem have different typing
-      provider,
-      signer,
-      {
-        apiEndpoint: restEndpoint,
-      },
-      walletClient
-    );
-  }, [walletClient, chainId, provider, signer]);
+		try {
+			return new HypercertExchangeClient(
+				chainId,
+				// @ts-expect-error - wagmi and viem have different typing
+				provider,
+				signer,
+				{
+					apiEndpoint: restEndpoint,
+				},
+				walletClient,
+			);
+		} catch (e) {
+			console.error("Failed to create an exchange client", e);
+		}
+	}, [walletClient, chainId, provider, signer]);
 
-  return { client };
+	return { client };
 };
